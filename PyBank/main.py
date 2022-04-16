5e86e237a1a4385e17f70625b27ece98aad467c0
jtell32 <101846606+jtell32@users.noreply.github.com>
2022-04-15 20:17:33 -0500

    updated main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,13 @@
 import os
+
 import csv
-csvpath = os.path.join("..", "Resources", "budget_data.csv")
+
+csvpath = os.path.join("..", "Resources", "budget_data.numbers")
 
 print("Financial Analysis")
     
 print("----------------------------------")
 
-for i 
+
+
 
